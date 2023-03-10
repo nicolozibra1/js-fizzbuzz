2f3d7b412c47c60c7473310cac4897f733d077a6
nicolozibra1 <nico.zibra@gmail.com>
2023-03-10 16:07:11 +0100

    script-calc-multiples

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const numSquare = 100;
 
-
-
-
 const grid = document.getElementById('grid');
+const FizzBuzz = "FizzBuzz";
+const fizz = "Fizz"
+const buzz = "Buzz"
 
 for (let i = 1; i <= numSquare; i++){
     grid.innerHTML += 
@@ -12,4 +12,30 @@ for (let i = 1; i <= numSquare; i++){
                 ${i}
             </div>
         </div>`;
+    if (i % 3 === 0 && i % 5 === 0){
+        grid.innerHTML +=
+        `<div class="square-box">
+            <div class="square">
+                ${FizzBuzz}
+            </div>
+        </div>`;
+    }
+    else if (i % 3 === 0){
+        grid.innerHTML +=
+        `<div class="square-box">
+            <div class="square">
+                ${fizz}
+            </div>
+        </div>`;
+    }
+    else if (i % 5 === 0){
+        grid.innerHTML +=
+        `<div class="square-box">
+            <div class="square">
+                ${buzz}
+            </div>
+        </div>`;
+    }
+
+    console.log(i);
 }
